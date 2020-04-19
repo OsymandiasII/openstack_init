07e20f75b03fda1848d40b932bf6364c5e4c1f2a
Ubuntu <Ozymandias@Openstack-test.0v5ixbvmftfujkemejwlqwhawg.bx.internal.cloudapp.net>
2020-04-19 16:04:58 +0000

    deploy.sh : little script to install microstack, download cento7 image, add the image to the list and launch an instance

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo snap install microstack --classic --channel=rocky/edge
+sudo microstack.init --auto
+wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-Azure-1703.qcow2
+microstack.openstack image create Centos7 --disk-format qcow2 --container-format bare --public --file CentOS-7-x86_64-Azure-1703.qcow2
+microstack.launch Centos7 --name test --flavor 2
